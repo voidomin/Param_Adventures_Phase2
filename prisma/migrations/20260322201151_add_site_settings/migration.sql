28ad26b109db05de642b3674c2c324956948a2c4
Akash Bhat <akashkbhat216@gmail.com>
2026-04-15 23:55:38 +0530

    chore: include missing migration history in repository

---
+++ prisma/migrations/20260322201151_add_site_settings/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "SiteSetting" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "SiteSetting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SiteSetting_key_key" ON "SiteSetting"("key");
